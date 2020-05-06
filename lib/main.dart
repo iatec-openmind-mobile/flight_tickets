f857af9d50c7331df85f2fdeab34ffe18fc6b73c
Mateus Felipe <mateusfccp@gmail.com>
2020-05-05 21:34:27 -0300

    Preparing for next iteration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:layout/shared/colors.dart';
-import 'package:layout/widgets/ticket_card.dart';
 
+import 'shared/colors.dart';
 import 'shared/flight_ticket.dart';
+import 'shared/ticket_card.dart';
+import 'shared/topbar.dart';
 
 void main() => runApp(MyApp());
 
@@ -39,12 +40,53 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColors.backgroundGrey,
-      body: ListView.separated(
-        padding: const EdgeInsets.all(24.0),
-        itemCount: tickets.length,
-        itemBuilder: (_, index) => TicketCard(ticket: tickets[index]),
-        separatorBuilder: (_, __) => const SizedBox(height: 12.0),
+      body: Stack(
+        children: <Widget>[
+          ListView.separated(
+            padding: EdgeInsets.fromLTRB(
+              24.0,
+              150.0 + MediaQuery.of(context).padding.top,
+              24.0,
+              15.0,
+            ),
+            itemBuilder: (_, index) => TicketCard(ticket: tickets[index]),
+            itemCount: tickets.length,
+            separatorBuilder: (_, __) => const SizedBox(height: 12),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Topbar(),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: IgnorePointer(
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      MyColors.backgroundGrey.withAlpha(0),
+                      Colors.white,
+                    ],
+                  ),
+                ),
+                height: 188.0,
+              ),
+            ),
+          )
+        ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.tune),
+        backgroundColor: MyColors.niceBlue,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
